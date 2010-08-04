9c384b724a4176538ccf75bb07b4a2367b5835c9
Freepine <freepine@gmail.com>
2010-08-04 11:16:55 +0800

    Add test for seeking to near the clip end.

--- MediaPlayerTest.cpp
+++ MediaPlayerTest.cpp
@@ -8,7 +8,9 @@
 #include "CppUTest/TestHarness.h"
 
 const char* SDCARD_LOCATION = "/mnt/sdcard/";
-const char* WMA_TESTFILE = "No_Woman_No_Cry_128K.wma";
+const char* WMA_TESTFILE = "Beethoven_2.wma";
+
+const int MAXIMUM_RESUME_DELAY_IN_MSEC = 2*1000;
 
 using namespace android;
 
@@ -16,9 +18,11 @@ TEST_GROUP(MediaPlayer)
 {
     sp<MediaPlayer> mediaplayer;
     sp<TestingMediaPlayerListener> listener;
+    int durationInMsec;
 
     void setup()
     {
+        durationInMsec = 0;
         String8 fileName(SDCARD_LOCATION);
         fileName += WMA_TESTFILE;
         mediaplayer = new MediaPlayer;
@@ -40,8 +44,45 @@ TEST_GROUP(MediaPlayer)
     {
         status_t retCode = mediaplayer->prepare();
         CHECK(OK == retCode);
+        retCode = mediaplayer->getDuration(&durationInMsec);
+        LOGV("Track duration: %d", durationInMsec);
+        CHECK(OK == retCode);
         retCode = mediaplayer->start();
         CHECK(OK == retCode);
+    }
+
+    void testSeek(int seekPosition)
+    {
+        const int SEEK_MAXIMUM_TOLERATION_IN_MSEC = 2*1000;
+        LOGV("seek to: %d", seekPosition);
+        if(seekPosition > durationInMsec)
+        {
+            LOGW("Seek beyond clip duration, durationInMsec=%d", durationInMsec);
+        }
+        status_t retCode = mediaplayer->seekTo(seekPosition);
+        CHECK(OK == retCode);
+        for(int i=0; i<10; i++)
+        {
+            if(listener->isSeekComplete())
+            {
+                int positionInMsec = getPosition();
+                LOGV("after seek, current position = %d", positionInMsec);
+                CHECK(positionInMsec + SEEK_MAXIMUM_TOLERATION_IN_MSEC > seekPosition);
+                CHECK(positionInMsec < SEEK_MAXIMUM_TOLERATION_IN_MSEC + seekPosition);
+                return;
+            }
+            usleep(200*1000);
+        }
+        FAIL("Seek didn't complete after 2 seconds!");
+    }
+
+    int getPosition()
+    {
+        int positionInMsec = 0;
+        status_t retCode = mediaplayer->getCurrentPosition(&positionInMsec);
+        CHECK(OK == retCode);
+        LOGV("current position is %d", positionInMsec);
+        return positionInMsec;
     }
 };
 
@@ -62,10 +103,6 @@ TEST(MediaPlayer, play)
 {
     const int PLAYBACK_MAXIMUM_EXTRA_TIME_IN_SECONDS = 5;
     testPlay();
-    int durationInMsec = 0;
-    status_t retCode = mediaplayer->getDuration(&durationInMsec);
-    LOGV("Track duration: %d", durationInMsec);
-    CHECK(OK == retCode);
     usleep(durationInMsec * 1000);
     sleep(PLAYBACK_MAXIMUM_EXTRA_TIME_IN_SECONDS);
     CHECK(true == listener->isPlaybackComplete());
@@ -79,44 +116,33 @@ TEST(MediaPlayer, pauseAndResume)
     sleep(PAUSE_POSITION);
     status_t retCode = mediaplayer->pause();
     CHECK(OK == retCode);
-    int positionInMsec = 0;
-    retCode = mediaplayer->getCurrentPosition(&positionInMsec);
-    CHECK(OK == retCode);
-    LOGV("current position is %d after pause", positionInMsec);
+    int positionInMsec = getPosition();
     sleep(TIME_INTERVAL);
-    int positionInMsec2 = 0;
-    retCode = mediaplayer->getCurrentPosition(&positionInMsec2);
-    LOGV("current position is %d after pause a while", positionInMsec2);
+    int positionInMsec2 = getPosition();
     CHECK(positionInMsec2 == positionInMsec);
     retCode = mediaplayer->start();
     CHECK(OK == retCode);
     sleep(TIME_INTERVAL);
-    int positionAfterResumeInMsec = 0;
-    retCode = mediaplayer->getCurrentPosition(&positionAfterResumeInMsec);
-    LOGV("current position is %d after resume several seconds", positionAfterResumeInMsec);
-    CHECK(positionAfterResumeInMsec > positionInMsec2 + (TIME_INTERVAL-1)*1000);
+    int positionAfterResumeInMsec = getPosition();
+    CHECK(positionAfterResumeInMsec > positionInMsec2 + TIME_INTERVAL*1000 - MAXIMUM_RESUME_DELAY_IN_MSEC);
 }
 
 TEST(MediaPlayer, seek)
 {
     const int SEEK_POSITION_IN_MSEC = 30*1000;
-    const int SEEK_MAXIMUM_TOLERATION_IN_MSEC = 2*1000;
+    const int TIME_IN_MSEC_BEFORE_EOS = 1*1000;
+    const int TIME_INTERVAL = 3;
     testPlay();
-    status_t retCode = mediaplayer->seekTo(SEEK_POSITION_IN_MSEC);
-    CHECK(OK == retCode);
-    for(int i=0; i<10; i++)
-    {
-        if(listener->isSeekComplete())
-        {
-            int positionInMsec = 0;
-            retCode = mediaplayer->getCurrentPosition(&positionInMsec);
-            LOGV("current position is %d after seeking to %d", positionInMsec, SEEK_POSITION_IN_MSEC);
-            CHECK(OK == retCode);
-            CHECK(positionInMsec + SEEK_MAXIMUM_TOLERATION_IN_MSEC > SEEK_POSITION_IN_MSEC);
-            CHECK(positionInMsec < SEEK_MAXIMUM_TOLERATION_IN_MSEC + SEEK_POSITION_IN_MSEC);
-            return;
-        }
-        usleep(200*1000);
-    }
-    FAIL("Seek didn't complete after 2 seconds!");
+    testSeek(SEEK_POSITION_IN_MSEC);
+    mediaplayer->pause();
+    int seekPosition = durationInMsec/2;
+    testSeek(seekPosition);
+    mediaplayer->start();
+    sleep(TIME_INTERVAL);
+    int positionAfterResumeInMsec = getPosition();
+    CHECK(positionAfterResumeInMsec > seekPosition + TIME_INTERVAL*1000 - MAXIMUM_RESUME_DELAY_IN_MSEC);
+    seekPosition = durationInMsec - TIME_IN_MSEC_BEFORE_EOS;
+    testSeek(seekPosition);
+    sleep((TIME_IN_MSEC_BEFORE_EOS + MAXIMUM_RESUME_DELAY_IN_MSEC)/1000);
+    CHECK(true == listener->isPlaybackComplete());
 }
